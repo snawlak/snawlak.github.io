49cab150735c0f89fe2e0ff8a4fe27c20638d959
Kuba <jakubpawlak3011@gmail.com>
2021-11-04 12:04:11 +0100

    photo - list

--- js/gallery.js
+++ js/gallery.js
@@ -1,25 +1,65 @@
-function initGallery() {
+function initGallery(src) {
     var pswpElement = document.querySelectorAll('.pswp')[0];
 
     // build items array
     var items = [
         {
             src: 'img/bg-home.jpg',
-            w: 600,
-            h: 400
-        },
-        {
-            src: 'https://placekitten.com/1200/900',
             w: 1200,
             h: 900
-        }
+        },
+        {
+            src: 'img/gallery/1.jpeg',
+            w: 1200,
+            h: 900
+        },
+        {
+            src: 'img/gallery/2.jpeg',
+            w: 1200,
+            h: 900
+        },
+        {
+            src: 'img/gallery/3.jpeg',
+            w: 1200,
+            h: 900
+        },
+        {
+            src: 'img/gallery/4.jpeg',
+            w: 1200,
+            h: 900
+        },
+        {
+            src: 'img/gallery/5.jpeg',
+            w: 1200,
+            h: 900
+        },
+        {
+            src: 'img/gallery/6.jpeg',
+            w: 1200,
+            h: 900
+        },
+        {
+            src: 'img/gallery/7.jpeg',
+            w: 1200,
+            h: 900
+        },
+        {
+            src: 'img/gallery/8.jpeg',
+            w: 1200,
+            h: 900
+        },
     ];
 
-    // define options (if needed)
+    var startIndex = 0;
+    for (var i = 0; i < items.length; i++) {
+        if (items[i].src === src){
+            startIndex = i;
+            break;
+        }
+    }
+
     var options = {
-        // optionName: 'option value'
-        // for example:
-        index: 0 // start at first slide
+        index: startIndex // start at first slide
     };
 
     // Initializes and opens PhotoSwipe
