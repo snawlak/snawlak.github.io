853f6e32add3de7c67dbf483367e7d24c287987f
Jakub Pawlak <jakp@netcompany.com>
2021-11-10 20:46:08 +0100

    popups

--- js/o-nas.js
+++ js/o-nas.js
@@ -51,7 +51,17 @@ var veneziaPopup =  "<div class='city-container'>"+
                     "   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"+
                     "   </p>"+
                     "</div>";
+var malmoPopup =    "<div class='city-container'>"+
+                    "   <h3 class='city-name'>"+
+                    "       Wenecja"+    
+                    "   </h3>"+
+                    "   <img src='img/gallery/6.jpeg' alt='venezia' class='city-img'>"+
+                    "   <p class='city-description'>"+
+                    "   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"+
+                    "   </p>"+
+                    "</div>";
 
 
 
 veneziaMarker.bindPopup(veneziaPopup).openPopup();
+malmoMarker.bindPopup(malmoPopup).openPopup();
